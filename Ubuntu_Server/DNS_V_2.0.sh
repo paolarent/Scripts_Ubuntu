b55dfaf3e0283ac9fb13d5dfe788a33fbdaaca06
paolarent <prenteriaf@gmail.com>
2025-02-22 00:33:26 -0700

    Update DNS_V_2.0.sh

--- Ubuntu_Server/DNS_V_2.0.sh
+++ Ubuntu_Server/DNS_V_2.0.sh
@@ -2,11 +2,11 @@
 echo "*********_CONFIGURAR DNS EN UBUNTU SERVER_************"
 
 #Importar las funciones desde sus archivos
-source ./F_Perdir_IP.sh
-source ./F_Validar_IP.sh
-source ./F_Ped_Val_Dominio_DNS.sh
-source ./F_IP_Estatica.sh
-source ./F_Config_DNS.sh
+source ./Funciones/F_Perdir_IP.sh
+source ./Funciones/F_Validar_IP.sh
+source ./Funciones/F_Ped_Val_Dominio_DNS.sh
+source ./Funciones/F_IP_Estatica.sh
+source ./Funciones/F_Config_DNS.sh
 
 #Pedir y validar la IP
 pedir_IP
