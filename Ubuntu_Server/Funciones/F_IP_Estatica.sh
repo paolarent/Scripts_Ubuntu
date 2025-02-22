0bc7d383b8aea186d5899624cc79e0b1a2135669
paolarent <prenteriaf@gmail.com>
2025-02-22 00:44:55 -0700

    Update F_IP_Estatica.sh

--- Ubuntu_Server/Funciones/F_IP_Estatica.sh
+++ Ubuntu_Server/Funciones/F_IP_Estatica.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 #Configurar la IP estática (SERVIDOR)
-configurar_ip_estatica() {
+configurar_ip_estatica(){
     local ip="$1"
     local ARCHIVO_NETPLAN="/etc/netplan/50-cloud-init.yaml"
     local ADAPTADOR2="enp0s8"
 
 sudo bash -c "cat > $ARCHIVO_NETPLAN" <<EOL
 network:
-version: 2
-ethernets:
+  version: 2
+  ethernets:
     enp0s3:
-    dhcp4: true
+      dhcp4: true
     $ADAPTADOR2:
-    dhcp4: no
-    addresses:
+      dhcp4: no
+      addresses:
         - $ip/24
-    nameservers:
+      nameservers:
         addresses:
-        - 8.8.8.8
-        - 1.1.1.1
+          - 8.8.8.8
+          - 1.1.1.1
 EOL
 
     echo "Configurando IP estática..."
